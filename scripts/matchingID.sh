54258dbd44c89fcf4aed80e8bcff0f2dc4d56cc5
kylie <133984183+kylidevi@users.noreply.github.com>
2025-05-14 09:46:34 -0300

    Creating first scripts to filter the IDs that I will be using for the analysis.

---
+++ scripts/matchingID.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=1-00:00
+#SBATCH --mem=1500G
+#SBATCH --mail-user=162298d@acadiau.ca
+#SBATCH --mail-type=ALL
+
+module load vcftools
+module load java
+
+vcftools --vcf /home/kdeville/projects/def-zmigicov/mig_lab/abc_wgs/gencove/data/master-vcf/Foodimprover.all.variants.new.vcf \
+--keep /home/kdeville/projects/def-zmigicov/mig_lab/honours_kylie/listIDs.txt \
+--out /home/kdeville/projects/def-zmigicov/mig_lab/honours_kylie/outputs/output_filtered_vcf
