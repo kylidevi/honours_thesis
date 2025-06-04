424173b507ce439d639858d2402e81bb83ac58c4
kylie <133984183+kylidevi@users.noreply.github.com>
2025-06-04 15:32:53 -0300

    running some of the traits seperately (PCs)

---
+++ code/20250604_GWAS_pc2.R
@@ -0,0 +1,33 @@
+# GWAS Analysis
+
+print("Loading in libraries")
+library(dplyr)
+library(tidyverse)
+library(GAPIT)
+
+print("Loading in HapMap file")
+wgs_data <- read.delim("/home/kdeville/projects/def-zmigicov/mig_lab/honours_kylie/outputs/SNPs_hapmap.hmp.txt", header = FALSE)
+
+
+print("Loading in trait data")
+trait_data <- read.csv("/home/kdeville/projects/def-zmigicov/mig_lab/honours_kylie/20250527_filtered_traitdata.csv")
+
+
+print("Data cleaning")
+# Genetic data | this associates all the wrongly set chromosomes to an additional chromosome 18
+wgs_data$V3[wgs_data$V3 == 0] <- 18
+
+# Trait data | selecting for the apple_id 
+## Running linear traits first (width - circularity)
+trait_data_limited <- trait_data %>%
+  select(4, 25)
+
+
+print("Running model")
+mlmm_results <- GAPIT(
+  Y = trait_data_limited,
+  G = wgs_data,
+  model = "MLMM",
+  SNP.MAF = 0.01,
+  PC.total = 3
+)
---
+++ scripts/20250604_GWAS_pc2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=3-00:00:00
+#SBATCH --mem=1510G
+#SBATCH --mail-user=162298d@acadiau.ca
+#SBATCH --mail-type=ALL
+
+module load r/4.4.0
+
+export R_LIBS_USER="/home/kdeville/projects/def-zmigicov/mig_lab/bin/RPackages"
+
+Rscript /home/kdeville/projects/def-zmigicov/mig_lab/honours_kylie/gapit_results_pc2/202506040529_GWAS_pc2.R
